bc5494836f52f2038ea77249812abc7729f0e9cf
Kevin Harris (CANADA) <kevinhar@microsoft.com>
2016-11-22 11:29:09 -0500

    Back to default title.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AppService Day - Express' });
+  res.render('index', { title: 'Express' });
 });
 
 // Required Dependencies
